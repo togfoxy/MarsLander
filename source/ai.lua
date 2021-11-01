71515f9bcf8c15f5f3fcbd40d50aea2f0562c691
Fox <togfoxy@users.noreply.github.com>
2021-11-01 23:53:21 +1000

    Add files via upload

--- source/ai.lua
+++ source/ai.lua
@@ -51,10 +51,11 @@ print("New base is " .. LanderObj.nextbaseid)
  
 	end
 	
-	if love.math.random(1,4) == 1 then
+	if love.math.random(1,10) == 1 then
 		-- exploratory
 		preferredangle = 180 + love.math.random(1,11) * 15
 		preferredthrust = love.math.random(1,2)
+print("Exploring")
 	
 	else
 		-- exploitive
@@ -77,18 +78,19 @@ print("New base is " .. LanderObj.nextbaseid)
 			strThrust = string.sub(key1, 4,4)
 			preferredthrust = tonumber(strThrust)
 
-			print("Agent option: " .. strTemp, preferredangle, preferredthrust, "score : " .. largestValue)
+			-- print("Agent option: " .. strTemp, preferredangle, preferredthrust, "score : " .. largestValue)
 		else
 			-- go random
 			-- exploratory
 			preferredangle = 180 + love.math.random(1,11) * 15
-			preferredthrust = love.math.random(1,2)			
+			preferredthrust = love.math.random(1,2)	
+print("Exploring")			
 		end		
 	end
 	
 	-- capture some stuff to determine rewards later
 	LanderObj.olddistance = math.abs(cf.GetDistance(LanderObj.x, LanderObj.y, LanderObj.nextbasex, LanderObj.nextbasey))
-	
+	LanderObj.previousfuel = LanderObj.fuel
 	LanderObj.previousdistance = mapdistance(cf.round(LanderObj.nextbasex - LanderObj.x,0))
 	LanderObj.previousy = mapy(LanderObj.y)
 	LanderObj.previousvx = mapvx(LanderObj.vx)
@@ -96,6 +98,7 @@ print("New base is " .. LanderObj.nextbaseid)
 	
 	LanderObj.previousangle = preferredangle
 	LanderObj.previousthrust = preferredthrust		-- 1 or 2
+	
 	
 	-- convert to boolean just prior to RETURN
 	if preferredthrust == 1 then 
@@ -108,16 +111,9 @@ end
 
 local function newComputeRewards(LanderObj)
 	
+	local totalreward = 0
 	local strTemp1 = tostring(LanderObj.previousdistance) .. tostring(LanderObj.previousy) .. tostring(LanderObj.previousvx) .. tostring(LanderObj.previousvy)
 	local strTemp2 = tostring(LanderObj.previousangle .. LanderObj.previousthrust)
-	
-	-- initialise QTable1
-	if QTable1 == nil then QTable1 = {} end
-	if QTable1[strTemp1] == nil then QTable1[strTemp1] = {} end
-	if QTable1[strTemp1][strTemp2] == nil then
-		QTable1[strTemp1][strTemp2] = {}
-		QTable1[strTemp1][strTemp2] = 0
-	end
 	
 	if LanderObj.preferredangleangle == nil then LanderObj.preferredangleangle = 270 end
 	if LanderObj.preferredthrust == nil then LanderObj.preferredthrust = 1 end
@@ -128,14 +124,14 @@ local function newComputeRewards(LanderObj)
 	
 	if newdistance < olddistance then
 		-- reward
-		QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + 1
+		totalreward = totalreward + 1
 	elseif newdistance > olddistance then
-		QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] - 1
+		totalreward = totalreward - 1
 	end
 	
 	-- is agent off the top of the screen?
 	if LanderObj.y <= 0 then
-		QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] - 1
+		totalreward = totalreward - 1
 	end
 	
 	-- did agent touch terrain?
@@ -143,35 +139,117 @@ local function newComputeRewards(LanderObj)
 	local LanderXValue = cf.round(LanderObj.x)
 	local groundYvalue = garrGround[LanderXValue]
 	if LanderObj.y > (groundYvalue - enum.constLanderImageYOffset) then		-- the offset is the size of the lander image
-		if LanderObj.x >= LanderObj.nextbasex and LanderObj.x <= LanderObj.nextbasex + 85 then
+		if Lander.isOnLandingPad(LanderObj, 2) then
 			-- on base
-			QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + 10
+			totalreward = totalreward + 1
 			LanderObj.fuel = LanderObj.fueltanksize
-			LanderObj.lastbaseid = nil
+			
+			-- but is it the right base?
+			if math.abs(LanderObj.nextbasex - LanderObj.x) < 200 then
+				totalreward = totalreward + 10
+				LanderObj.lastbaseid = nil
+			end
 		else
 			-- off base
-			QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] - 10
+			totalreward = totalreward - 10
 		end
 	end
 
 	-- is bad motion being corrected?
 	if LanderObj.x > LanderObj.nextbasex and LanderObj.vx > 0 and LanderObj.preferredangleangle <= 270 and LanderObj.preferredthrust == 2 then
-		QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + 1
+		totalreward = totalreward + 1
 	end
 	if LanderObj.x < LanderObj.nextbasex and LanderObj.vx < 0 and LanderObj.preferredangleangle >= 270 and LanderObj.preferredthrust == 2 then
-		QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + 1
+		totalreward = totalreward + 1
 	end	
 	
-	if QTable1[strTemp1][strTemp2] > 30 then QTable1[strTemp1][strTemp2] = 30 end
-	fun.SaveQTable1()
+	-- is lander fuel efficient?
+	if LanderObj.previousfuel ~= nil then
+		local fuelused = LanderObj.previousfuel - LanderObj.fuel
+		local distancemoved = olddistance - newdistance
+		local distperfuel = cf.round(distancemoved / fuelused, 0)
+		
+		-- globals
+		gfltfuelused = gfltfuelused + fuelused
+		gfltdistancemoved = gfltdistancemoved + distancemoved
+		gfltavgdistperfuel = cf.round(gfltdistancemoved / gfltfuelused)
+		
+-- print(gfltavgdistperfuel)
+		
+		if fuelused == 0 then distperfuel = 99 end
+		if distancemoved == 0 then distperfuel = -99 end
 
+		if distperfuel < 0 then
+			--totalreward = totalreward - 1
+		end
+		if distperfuel > 0 then 
+			totalreward = totalreward + 1
+		end
+		if distperfuel > gfltavgdistperfuel then
+			totalreward = totalreward + 1
+		end
+	end
+	
+	-- commit learnings to table
+
+	if QTable1[strTemp1] == nil then QTable1[strTemp1] = {} end
+	if QTable1[strTemp1][strTemp2] == nil then
+		QTable1[strTemp1][strTemp2] = {}
+		QTable1[strTemp1][strTemp2] = 0
+	end	
+	
+	-- QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + totalreward
+	
+	-- add steps to long memory
+	if garrAIHistory == nil then garrAIHistory = {} end
+	
+	local strTemp3 = strTemp1 .. ":" .. strTemp2
+	mystep = {}
+	mystep.key = strTemp3
+	mystep.reward = totalreward
+	table.insert(garrAIHistory, mystep)
+
+	-- reward long memory
+	if #garrAIHistory > 2 then
+		-- check the reward over the last 3 steps
+		local tempreward = 0
+		local lowestreward = math.min(garrAIHistory[1].reward,garrAIHistory[2].reward,garrAIHistory[3].reward)
+		
+		if lowestreward < 0 then
+			-- at least one thing bad happened.
+			-- punish the whole chain
+			tempreward = lowestreward
+		else
+			tempreward = garrAIHistory[1].reward + garrAIHistory[2].reward + garrAIHistory[3].reward
+		end
+		
+print("Reward = " .. tempreward)		
+		
+		-- now loop and apply the reward
+		for i = 1,3 do
+			-- unpack the state and action
+			local pos = string.find(garrAIHistory[i].key, ":")
+			strTemp1 = string.sub(garrAIHistory[i].key, 1, pos - 1)
+			strTemp2 = string.sub(garrAIHistory[i].key, pos + 1)
+			QTable1[strTemp1][strTemp2] = QTable1[strTemp1][strTemp2] + tempreward
+		end
+		
+		table.remove(garrAIHistory, 1)		-- remove the oldest entry
+	end
+	
+	if QTable1[strTemp1][strTemp2] > 50 then QTable1[strTemp1][strTemp2] = 50 end
+	if QTable1[strTemp1][strTemp2] < -50 then QTable1[strTemp1][strTemp2] = -50 end
+	
+	fun.SaveQTable1()
 end
 
 function ai.DoAI(LanderObj, dt)
 
-	if QTable1 == nil then
-		QTable1 = {}
-	end
+	-- initialise QTable1
+	if QTable1 == nil then QTable1 = {} end
+	if gfltfuelused == nil then gfltfuelused = 0 end
+	if gfltdistancemoved == nil then gfltdistancemoved = 0 end
+	if gfltavgdistperfuel == nil then gfltavgdistperfuel = 0 end
 
 	LanderObj.aitimer = LanderObj.aitimer - dt
 	if LanderObj.aitimer <= 0 and LanderObj.fuel > 1 then
