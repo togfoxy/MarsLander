5deed0b3c29872057be8b816fe96f71cb02c9f94
Lars Loe <madbyte@tuta.io>
2021-11-06 21:38:40 +0100

    Improve ship module handling // minor code clean-up
    Goal was to get rid of global module constants and instead manage them in a more dynamic way.
    
    While I was on it I made some changes to the shop menu and lander code to reflect the changes and make it easier to add new modules.

--- source/enum.lua
+++ source/enum.lua
@@ -14,15 +14,3 @@ basetypeFuel = 2
 basetypeBuilding1 = 7
 
 baseMaxFuel = 15
-
-moduleCostsThrusters = 225
-moduleCostsLargeTank = 200
-moduleCostsRangeFinder = 175
-moduleCostSideThrusters = 185
-
-moduleNamesThrusters = "Fuel efficient thrusters"
-moduleNamesLargeTank = "Large tank"
-moduleNamesRangeFinder = "Rangefinder"
-moduleNamesSideThrusters = "Side thrusters"
-
-moduleMassSideThrusters = 20
