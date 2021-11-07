dd399cdeee74c3a63cc0d1bc22ab4af3b1846b3f
Fox <togfoxy@users.noreply.github.com>
2021-11-07 11:23:54 +1000

    Had to roll back some code to fix the save game bug
    Fixes #173

--- source/objects/lander.lua
+++ source/objects/lander.lua
@@ -315,9 +315,14 @@ function Lander.create()
 	local lander = {}
 	lander.x = gintOriginX
 	lander.y = garrGround[lander.x] - 8
-	lander.sprite = garrImages[5]
-	lander.width = lander.sprite:getWidth()
-	lander.height = lander.sprite:getHeight()
+	--lander.sprite = garrImages[5]
+	--lander.width = lander.sprite:getWidth()
+	--lander.height = lander.sprite:getHeight()
+	
+	lander.spriteenum = enum.imageShip
+	lander.width = garrImages[lander.spriteenum]:getWidth()
+	lander.height = garrImages[lander.spriteenum]:getHeight()
+
 	-- 270 = up
 	lander.angle = 270
 	lander.vx = 0
@@ -433,7 +438,7 @@ end
 
 function Lander.draw(worldOffset)
 	-- draw the lander and flame
-	for landerId, lander in ipairs(garrLanders) do
+	for landerId, lander in pairs(garrLanders) do
 		local sx, sy = 1.5, 1.5
 		local drawingX = lander.x - worldOffset
 		local drawingY = lander.y
@@ -445,8 +450,10 @@ function Lander.draw(worldOffset)
 			love.graphics.setColor(1,1,1,0.5)
 		end
 
+		-- TODO: work out why lander.width doesn't work in mplayer mode
 		local ox = lander.width / 2
 		local oy = lander.height / 2
+		
 		love.graphics.draw(garrImages[5], drawingX,drawingY, math.rad(lander.angle), sx, sy, ox, oy)
 
 		--[[
@@ -457,7 +464,7 @@ function Lander.draw(worldOffset)
 			file and just use them here without the local keyword.
 		--]]
 		-- draw flames
-		local flameSprite	= garrImages[4]
+		local flameSprite	= garrImages[enum.imageFlameSprite]
 		local flameWidth	= flameSprite:getWidth()
 		local flameHeight	= flameSprite:getHeight()
 		local ox 			= flameWidth / 2
