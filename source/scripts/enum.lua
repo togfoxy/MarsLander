2f3a9f9949e24753b6615b3a839bfee8ce5526ac
Lars Loe <madbyte@tuta.io>
2021-11-07 19:17:19 +0100

    minor enum.lua clean-up

--- source/scripts/enum.lua
+++ source/scripts/enum.lua
@@ -4,23 +4,23 @@ module(...,package.seeall)
 constSmokeTimer = 0.5
 constVYThreshold = 0.60
 constGravity = 0.6
-constSocketClientRate = 0.04		-- how frequently will client send to the host?
+-- how frequently will client send to the host?
+constSocketClientRate = 0.04
+
 constSocketHostRate = 0.1
 
 
 -- enumerators --
-
 basetypeFuel = 2
-basetypeBuilding = 6		-- this is when we don't care about building1 or building2 i.e. any building (but not fuel)
+
+-- this is when we don't care about building1 or building2 i.e. any building (but not fuel)
+basetypeBuilding = 6
+
 basetypeBuilding1 = 7
 basetypeBuilding2 = 8
 
 baseMaxFuel = 15
 
--- sprites and images -- 
-
-imageFlameSprite = 4
-imageShip = 5
 
 
 
