954928b4958f74c8f8d3bb569326ae9f6c5f24f1
Lars Loe <madbyte@tuta.io>
2021-11-10 16:16:01 +0100

    Changed module name to PascalCase in functions.loadGame

--- source/functions.lua
+++ source/functions.lua
@@ -150,7 +150,7 @@ function functions.LoadGame()
 
 	if error then
 		-- a file is missing, so display a popup on a new game
-		lovelyToasts.show("ERROR: Unable to load game!", 3, "middle")
+		LovelyToasts.show("ERROR: Unable to load game!", 3, "middle")
 	end
 end
 
